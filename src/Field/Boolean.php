9f94c3cbaca6eb82712a66e0d2853cde59db890c
Frederik Clasen <Frederik.clasen@bonnier.dk>
2025-01-09 19:05:26 +0100

    Added files from acf package, and changed namespaces

---
+++ src/Field/Boolean.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Corcel\Field;
+
+use Corcel\FieldInterface;
+
+/**
+ * Class Boolean.
+ *
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class Boolean extends Text implements FieldInterface
+{
+    /**
+     * @return bool
+     */
+    public function get()
+    {
+        return (bool) parent::get();
+    }
+}
